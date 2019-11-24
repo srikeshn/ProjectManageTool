eaadce981059d3e5fc4412951a8044d34fbec04f
srikesh <srikesh.n@gmail.com>
2019-11-24 16:22:38 +0530

    first commit

---
+++ FullStack/ReactSpringBoot/src/main/java/com/fullstack/ReactSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootApplication.class, args);
+	}
+
+}
